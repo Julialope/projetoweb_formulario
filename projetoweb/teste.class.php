b9633a1eed639c227cb2088712136ebefe4f6341
Julia Lopes <126586602+Julialope@users.noreply.github.com>
2023-04-05 16:16:39 -0300

    Update teste.class.php

--- projetoweb/teste.class.php
+++ projetoweb/teste.class.php
@@ -55,7 +55,7 @@ $pessoa->setObservacao($_POST['observacao']);
               <th width=8%>Observação</th>
               <th width=8%>Ação</th>
         </tr>
-        <tr align=center bgcolor=#007BFF>
+        <tr align=center bgcolor=#white>
               <th width=8%><?php echo $pessoa->getNome();?></th>
               <th width=8%><?php echo $pessoa->getTelefone();?></th>
               <th width=8%><?php echo $pessoa->getOrigem();?></th>
